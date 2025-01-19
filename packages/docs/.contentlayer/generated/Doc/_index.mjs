48ec75cb1abaac340cff78c87c16d535bba3e1c0
ryanmio <ryan@mioduski.us>
2025-01-19 09:51:13 -0500

    docs: add API reference documentation

--- packages/docs/.contentlayer/generated/Doc/_index.mjs
+++ packages/docs/.contentlayer/generated/Doc/_index.mjs
@@ -2,5 +2,10 @@
 
 import overview__indexMdx from './overview__index.mdx.json' assert { type: 'json' }
 import gettingStarted__indexMdx from './getting-started__index.mdx.json' assert { type: 'json' }
+import apiReference__indexMdx from './api-reference__index.mdx.json' assert { type: 'json' }
+import apiReference__envelopeOperationsMdx from './api-reference__envelope-operations.mdx.json' assert { type: 'json' }
+import apiReference__documentOperationsMdx from './api-reference__document-operations.mdx.json' assert { type: 'json' }
+import apiReference__customDocumentOperationsMdx from './api-reference__custom-document-operations.mdx.json' assert { type: 'json' }
+import apiReference__templateOperationsMdx from './api-reference__template-operations.mdx.json' assert { type: 'json' }
 
-export const allDocs = [overview__indexMdx, gettingStarted__indexMdx]
+export const allDocs = [overview__indexMdx, gettingStarted__indexMdx, apiReference__indexMdx, apiReference__envelopeOperationsMdx, apiReference__documentOperationsMdx, apiReference__customDocumentOperationsMdx, apiReference__templateOperationsMdx]
