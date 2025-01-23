4b81cc68eb26ed07888928a55de6b55b7756c681
ryanmio <ryan@mioduski.us>
2025-01-22 19:21:55 -0500

    basic config

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  external: ['react', 'react-dom'],
+  injectStyle: true,
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client";',
+    }
+  },
+}) 
