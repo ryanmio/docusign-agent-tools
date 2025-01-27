8be664195cc8375dde7e993d982076bf1b4f48ea
ryanmio <ryan@mioduski.us>
2025-01-27 11:30:38 -0500

    add todo in comment

--- packages/core/src/recipients.ts
+++ packages/core/src/recipients.ts
@@ -3,8 +3,17 @@
  */
 
 /**
- * Gather recipient information
+ * Gather recipient information for template or custom document sending
+ * @param roles Array of role definitions required for the document
+ * @param mode Whether this is for a template or custom document
+ * @returns Promise resolving to recipient information
+ * 
+ * Technical Debt:
+ * - Implement role validation
+ * - Add email format validation
+ * - Support custom routing order
+ * - Add recipient tabs configuration
  */
 export const defineRecipients = async (roles: any[], mode: 'template' | 'custom') => {
-  // TODO: Implement recipient definition
+  throw new Error('Recipient definition not yet implemented');
 } 
